7fd0f570711839745c735b58d9086bd88851f41b
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-01-05 20:11:53 +0200

    First screen

--- HomeWorkLesson2.3/ViewController.swift
+++ HomeWorkLesson2.3/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userNameField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    
+    private let user = "User"
+    private let password = "Password"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func forgotUserName(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Oops!", message: "Your name is \(user)", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default) { (action) in }
+        
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func forgotPassword(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Oops!", message: "Your password is \(password)", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default) { (action) in }
+        
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
